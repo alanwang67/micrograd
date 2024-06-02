153376b8aded17c1383c4c8adc4b7aba45498017
alanwang67 <alanwang599@gmail.com>
2024-06-02 19:19:25 -0400

    added params function for all strucuts

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,11 @@ struct ValueData {
 struct Value(Rc<RefCell<ValueData>>);
 
 impl Value {
-    fn new(data: f64, gradient: f64, children: Vec<Value>) -> Self {
+    fn new(data: f64) -> Self {
         Value(Rc::new(RefCell::new(ValueData {
             data, 
-            gradient,
-            children})))
+            gradient: 0.0,
+            children: vec![]})))
        
     }
 
@@ -58,7 +58,8 @@ impl Value {
         Value(Rc::new(RefCell::new(ValueData { data: self_data, gradient: 0.0, children: vec![Value(Rc::clone(&self.0))]})))
     }
 
-    fn backward(& self) {
+    // this will recurse infinitley if there are any overlaps 
+    fn backward(&self) {
         let self_gradient = self.0.borrow().gradient; 
         for node in &self.0.borrow().children {
             let node_gradient = node.0.borrow_mut().gradient;
@@ -75,6 +76,7 @@ impl Value {
 //     }
 // }
 
+// do we want to wrap this neuron within a rc<refcell> 
 #[derive(Debug)]
 struct Neuron {
     weights: Vec<Value>, 
@@ -88,16 +90,16 @@ impl Neuron {
         Neuron{
             weights: Vec::from_iter(0..n).iter().map(|_| { 
                 let data = rand::thread_rng().gen_range(-1.0..=1.0);
-                Value::new(data, 0.0, vec![])
+                Value::new(data)
 
             }).collect(),
-            bias: Value::new(0.0, 0.0, vec![]),
+            bias: Value::new(0.0),
             parameters: Vec::new(),
             nonlin: nonlin, 
         }
     }   
     
-    fn call(&self, n: Vec<Value>) -> Value {
+    fn call(&self, n: &Vec<Value>) -> Value {
         let mut acc = Value(Rc::clone(&self.bias.0));
         
         for (x, y) in (self.weights).iter().zip(n.iter()) {
@@ -123,11 +125,79 @@ impl Neuron {
 
 }
 
+#[derive(Debug)]
+struct Layer {
+    neurons: Vec<Neuron>
+}
+
+impl Layer {
+    fn new(nin: u64, nout: u64, nonlin: bool) -> Self {
+        Layer { 
+            neurons: Vec::from_iter(0..nout).iter().map(|_| Neuron::new(nin, nonlin)).collect(),
+        }
+    }
+
+    fn call(&self, x: &Vec<Value>) -> Vec<Value> {
+        self.neurons.iter().map(|neuron| {neuron.call(x)}).collect::<Vec<Value>>()
+    }
+
+    fn params(&self) -> Vec<Value> {
+        let mut v = vec![];
+        for n in &self.neurons { 
+            for p in &n.params() {
+                v.push(Value(Rc::clone(&p.0)));
+            }
+        }
+        v
+    }
+}
+
+struct MLP {
+    layers: Vec<Layer>
+}
+
+impl MLP {
+    // I think we need to put mutable on the second parameter 
+    // because it specifies what we can do with our vector even though
+    // we have ownership of it (something we have ownership over doesn't mean it's mutable)
+    fn new(&self, nin: u64, mut nout: Vec<u64>) -> Self { 
+        let mut sz = vec![nin];
+        sz.append(&mut nout);
+        let mut layers = vec![];
+        let mut i = 0;
+
+        while i < nout.len() {
+            layers.push(Layer::new(*&sz[i], *&sz[i+1], i != (nout.len()-1)));
+        }
+
+        MLP {
+            layers: layers
+        }
+    }
+
+    fn call(&self, mut x: Vec<Value>) -> Vec<Value> {
+        for layer in &self.layers {
+            x = layer.call(&x);
+        }
+        x
+    }
+
+    fn params(&self) -> Vec<Value> {
+        let mut v = vec![];
+        for l in &self.layers { 
+            for p in &l.params() {
+                v.push(Value(Rc::clone(&p.0)));
+            }
+        }
+        v
+    }
+
+}
 
 fn main() {
     // for now this will live on the stack 
-    let v1 = Value::new(2.0, 0.0, Vec::new());
-    let v2 = Value::new(6.0, 0.0, Vec::new());
+    let v1 = Value::new(2.0);
+    let v2 = Value::new(6.0);
 
     // let mut new_node = (&mut v).multiplication(&mut v1);
     // new_node.gradient = 1.0;
@@ -142,9 +212,15 @@ fn main() {
     // new_node.0.borrow_mut().gradient = 2.0;
     // new_node.params();
     
+    let x = Value::new(-4.0);
+    let z = ((Value::new(2.0).multiply(&x)).add(&Value::new(2.0))).add(&x);
+    z.0.borrow_mut().gradient = 1.0;
+    z.backward();
 
 
-    
+
+    println!("{:#?}", z)
+
 
     // println!("Now {:#?} will print!", &new_node);
     // new_node.children.get(0).unwrap().data = 0.2; 
