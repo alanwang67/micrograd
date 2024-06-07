a6893ab4487ad1c52665316044be315be87f15ec
alanwang67 <alanwang599@gmail.com>
2024-06-07 18:25:49 -0400

    working ish MLP

--- src/engine.rs
+++ src/engine.rs
@@ -2,10 +2,13 @@ use std::{cell::RefCell, rc::Rc, fmt, collections::HashSet};
 use by_address::ByAddress;
 
 #[derive(Debug)]
-pub struct ValueData {
+
+pub struct ValueData 
+{
     pub data: f64, 
     pub gradient: f64, 
     pub children: Vec<Value>,
+    pub backward: Option<fn(Value) -> ()>,
 }
 
 #[derive(Debug)]
@@ -22,7 +25,9 @@ impl Value {
         Value(Rc::new(RefCell::new(ValueData {
             data, 
             gradient: 0.0,
-            children: vec![]})))
+            children: vec![],
+            backward: Option::None,
+        })))
     }
 
     pub fn get_value(&self) -> f64 {
@@ -36,27 +41,36 @@ impl Value {
     pub fn multiply(&self, other: &Value) -> Self {
         let self_data = self.0.borrow().data; 
         let other_data = other.0.borrow().data;
-        self.0.borrow_mut().gradient += other_data;
-        other.0.borrow_mut().gradient += self_data;
 
-        Value(Rc::new(RefCell::new(ValueData { data: self_data * other_data, gradient: 0.0, children: vec![Value(Rc::clone(&self.0)), Value(Rc::clone(&other.0))]})))
+        let backward_fn = |out_value: Value| {
+            let children = &out_value.0.borrow().children;
+            let out_gradient = out_value.get_gradient();
+
+            let left_data = (&children[0]).0.borrow().data;
+            let right_data = (&children[1]).0.borrow().data;
+
+            (&children[0]).0.borrow_mut().gradient += right_data * out_gradient;
+            
+            (&children[1]).0.borrow_mut().gradient += left_data * out_gradient;
+        };
+
+        Value(Rc::new(RefCell::new(ValueData { data: self_data * other_data, gradient: 0.0, children: vec![Value(Rc::clone(&self.0)), Value(Rc::clone(&other.0))], backward: Option::Some(backward_fn) })))
     }
 
     pub fn add(&self, other: &Value) -> Self {
         let self_data = self.0.borrow().data; 
         let other_data = other.0.borrow().data;
-        self.0.borrow_mut().gradient += 1.0;
-        other.0.borrow_mut().gradient += 1.0;
+        
+        let backward_fn = |out_value: Value| {
+            let children = &out_value.0.borrow().children;
+            let out_gradient = out_value.get_gradient();
 
-        Value(Rc::new(RefCell::new(ValueData { data: self_data + other_data, gradient: 0.0, children: vec![Value(Rc::clone(&self.0)), Value(Rc::clone(&other.0))]})))
-    }
+            (&children[0]).0.borrow_mut().gradient += out_gradient;
+            
+            (&children[1]).0.borrow_mut().gradient += out_gradient;
+        };
 
-    pub fn power(&self, other: &Value) -> Self {
-        let self_data = self.0.borrow().data; 
-        let other_data = other.0.borrow().data as i32; 
-        self.0.borrow_mut().gradient += f64::from(other_data) * f64::powi(self_data, other_data - 1);
-
-        Value(Rc::new(RefCell::new(ValueData { data: f64::powi(self_data, other_data), gradient: 0.0, children: vec![Value(Rc::clone(&self.0))]})))
+        Value(Rc::new(RefCell::new(ValueData { data: self_data + other_data, gradient: 0.0, children: vec![Value(Rc::clone(&self.0)), Value(Rc::clone(&other.0))], backward: Option::Some(backward_fn) })))
     }
 
     pub fn subtract(&self, other: &Value) -> Self {
@@ -64,12 +78,43 @@ impl Value {
         self.add(other)
     }
 
+    pub fn power(&self, other: &Value) -> Self {
+        let self_data = self.0.borrow().data; 
+        let other_data = other.0.borrow().data as i32; 
+        self.0.borrow_mut().gradient += f64::from(other_data) * f64::powi(self_data, other_data - 1);
+
+
+        let backward_fn = |out_value: Value| {
+            let children = &out_value.0.borrow().children;
+            let left_data = (&children[0]).0.borrow().data;
+            let right_data = (&children[1]).0.borrow().data as i32;
+            let out_gradient = out_value.get_gradient();
+
+            (&children[0]).0.borrow_mut().gradient += f64::from(right_data) * f64::powi(left_data, right_data - 1) * out_gradient;
+        };
+
+        Value(Rc::new(RefCell::new(ValueData { data: f64::powi(self_data, other_data), gradient: 0.0, children: vec![Value(Rc::clone(&self.0)), Value(Rc::clone(&other.0))], backward: Option::Some(backward_fn)})))
+    }
+
+
     pub fn relu(&self) -> Self {
         let self_data = self.0.borrow().data; 
         let data = if self_data < 0.0 {0.0} else {self_data};
-        let self_grad = if self_data > 0.0 {1.0} else {0.0};
-        self.0.borrow_mut().gradient += self_grad;
-        Value(Rc::new(RefCell::new(ValueData { data: data, gradient: 0.0, children: vec![Value(Rc::clone(&self.0))]})))
+
+        let backward_fn = |out_value: Value| {
+            let children = &out_value.0.borrow().children;
+            let out_gradient: f64 = out_value.get_gradient();
+
+            let child_data = (&children[0]).0.borrow().data;
+
+            if child_data > 0.0 {
+                (&children[0]).0.borrow_mut().gradient += out_gradient;
+            } else {
+                (&children[0]).0.borrow_mut().gradient += 0.0;
+            }
+        };
+
+        Value(Rc::new(RefCell::new(ValueData { data: data, gradient: 0.0, children: vec![Value(Rc::clone(&self.0))], backward: Option::Some(backward_fn) })))
     }
 
     fn topological_sort(element: Value, topo: &mut Vec<Value>, visited: &mut HashSet<ByAddress<Rc<RefCell<ValueData>>>>) { 
@@ -85,7 +130,7 @@ impl Value {
         }
     }
 
-    pub fn backward(&self) {
+    pub fn propogate_backward(&self) {
         let mut topo = vec![]; 
         let mut visited = HashSet::new();
         Self::topological_sort(Value(Rc::clone(&self.0)), &mut topo, &mut visited);
@@ -94,9 +139,9 @@ impl Value {
         self.0.borrow_mut().gradient = 1.0;
         
         for element in &topo {
-            let element_gradient = element.0.borrow().gradient;   
-            for node in &element.0.borrow().children {
-                node.0.borrow_mut().gradient *= element_gradient;
+            match element.0.borrow().backward {
+                None => (),
+                Some(func) => func(Value(Rc::clone(&element.0)))
             }
         }
     }
@@ -115,6 +160,12 @@ mod tests {
         let result = (y.multiply(&x)).add(&(z.add(&x)));
 
         assert!(result.get_value() == -10.0);
+
+        let x = Value::new(8.0);
+        let y = Value::new(2.0);
+        let result = x.subtract(&y); 
+
+        assert!(result.get_value() == 6.0);
     }
 
     #[test]
@@ -123,8 +174,10 @@ mod tests {
         let y = Value::new(2.0);
         let z = Value::new(2.0);
         let result = (y.multiply(&x)).add(&(z.add(&x))); // y * x + z + x
-        result.backward();
 
+        result.propogate_backward();
+
+        println!("{:#?}", result);
         assert!(result.get_gradient() == 1.0);
         assert!(x.get_gradient() == 3.0);
         assert!(y.get_gradient() == -4.0);
--- src/main.rs
+++ src/main.rs
@@ -3,59 +3,43 @@ mod engine;
 mod nn;
 
 use crate::engine::Value;
-use crate::nn::{Neuron, Layer, MLP};
+use crate::nn::{MLP};
 
 fn main() {
-    let mut xs = vec![vec![Value::new(2.0), Value::new(3.0), Value::new(-1.0)], vec![Value::new(3.0), Value::new(-1.0), Value::new(0.5)], vec![Value::new(0.5), Value::new(1.0), Value::new(1.0)], vec![Value::new(1.0), Value::new(1.0), Value::new(-1.0)]];
-    let mut ys = vec![Value::new(1.0), Value::new(-1.0), Value::new(-1.0), Value::new(1.0)];
-
-    let n = MLP::new(3, vec![4,4,1]);
-    let mut data = vec![];
-    let mut loss = Value::new(0.0);
-
-    for x in xs {
-        data.push(n.call(x).pop().unwrap()); // assuming only 1 element in vector
-    }
-    
-    println!("{:#?}", data.iter().map(|value| value.0.borrow().data).collect::<Vec<f64>>());
-    
-    for (x,y) in data.iter().zip(ys.iter()) {
-        let squared_difference = (y.subtract(x)).power(&Value::new(2.0));
-        loss.add(&squared_difference);
-    }
-
-    println!("{:#?}", loss.backward());
-
     let mut i = 0;
-    while i < 1 { 
-        for p in n.params() {
-            let p_data = p.0.borrow().data;
-            p.0.borrow_mut().data += -0.01 * p_data;
+    let n = MLP::new(3, vec![4,4,1]);
+
+    while i < 1000 {    
+        let xs: Vec<Vec<Value>> = vec![vec![Value::new(2.0), Value::new(3.0), Value::new(-1.0)], vec![Value::new(3.0), Value::new(-1.0), Value::new(0.5)], vec![Value::new(0.5), Value::new(1.0), Value::new(1.0)], vec![Value::new(1.0), Value::new(1.0), Value::new(-1.0)]];
+        let predict: Vec<Value> = vec![Value::new(1.0), Value::new(-1.0), Value::new(-1.0), Value::new(1.0)];
+
+        let mut data: Vec<Value> = vec![];
+        let mut loss = Value::new(0.0);
+
+        for x in xs {
+            data.push(n.call(x).pop().unwrap()); // assuming only 1 element in vector
         }
 
-        loss.backward()
+        println!("current predictions: {:#?}", data.iter().map(|value| value.0.borrow().data).collect::<Vec<f64>>());
+
+        for (x,y) in data.iter().zip(predict.iter()) {
+            let squared_difference = (y.subtract(x)).power(&Value::new(2.0));
+            loss = loss.add(&squared_difference);
+        }
+
+        println!("current loss: {:#?}", loss.get_value());
 
         for p in n.params() {
-            let p_data = p.0.borrow().data;
-            p.0.borrow_mut().data += -0.01 * p_data;
+            p.0.borrow_mut().gradient = 0.0;
         }
+
+        loss.propogate_backward();
+
+        for p in n.params() {
+            let p_grad = p.get_gradient();
+            p.0.borrow_mut().data += 0.01 * p_grad;
+        }
+
         i += 1;
     }
-
-    let mut xs = vec![vec![Value::new(2.0), Value::new(3.0), Value::new(-1.0)], vec![Value::new(3.0), Value::new(-1.0), Value::new(0.5)], vec![Value::new(0.5), Value::new(1.0), Value::new(1.0)], vec![Value::new(1.0), Value::new(1.0), Value::new(-1.0)]];
-
-    for x in xs {
-        data.push(n.call(x).pop().unwrap()); // assuming only 1 element in vector
-    }
-
-    println!("{:#?}", data.iter().map(|value| value.0.borrow().data).collect::<Vec<f64>>());
-
-    // let value = MLP::new(3, vec![4,4,1]).call(x);
-    // println!("{:#?}", &value[0].0.borrow().data);
-
-    // let layer = Layer::new(2,3,true);
-    // println!("{:#?}", layer.call(&x).iter().map(|value| value.0.borrow().data).collect::<Vec<f64>>());
-
-
-    
 }
